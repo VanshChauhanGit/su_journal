4d7dfaaf6346b608217b41563096d5c38a24f525
VanshChauhanGit <vanshchauhan1106@gmail.com>
2025-09-11 22:08:40 +0530

    feat: update page components to use Wrapper and enhance layout

--- app/current-issue/page.js
+++ app/current-issue/page.js
@@ -1,7 +1,13 @@
+import Wrapper from "@/components/Wrapper";
 import React from "react";
 
 function page() {
-  return <div>Current Issue</div>;
+  return (
+    <Wrapper>
+      <h2 className="text-teal-700 text-2xl font-bold">Current Issue</h2>
+      <hr className="border-t border-teal-700 my-2" />
+    </Wrapper>
+  );
 }
 
 export default page;
