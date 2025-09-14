fad7ffd54f70ae50fea3f5a71a64df10b6693793
VanshChauhanGit <vanshchauhan1106@gmail.com>
2025-09-14 09:23:35 +0530

    feat: update content for the current issue of NICE Journal of Business, enhancing descriptions and topics covered

--- app/editor-desk/page.js
+++ app/editor-desk/page.js
@@ -10,138 +10,129 @@ function page() {
       <hr className="border-t border-teal-700 my-2" />
       <div className="col-md-12 content-wrapper space-y-3 text-justify">
         <p>
-          Welcome to the new issue of NICE Journal of Business (Vol. 17, Nos. 1
-          &amp; 2 : January - December, 2022). Each issue of the journal brings
-          to you the latest and authoritative insights into the dynamic and
-          fascinating world of business.
+          It is my pleasure to share with you the current issue of NICE Journal
+          of Business (Vol. 18, No. 1: January-June 2023). The journal provides
+          an outlet to researchers and scholars working in the field of
+          business. Each issue of the journal brings to you the latest and
+          authoritative insights into the dynamic and fascinating world of
+          business. I hope you enjoy reading the contents of this issue.
         </p>
 
         <p>
-          I am pleased to report that this issue contains one Perspective, six
-          Research papers, and two pieces under Communication, reflecting
-          diverse interests in the vast field of business.
+          In this issue, we have put together one perspective, seven research
+          papers, and two pieces of communication reflecting diverse interests
+          in the vast field of business.
         </p>
 
         <p>
-          The issue has topics like the impact of Russia's invasion of Ukraine
-          on world economy, influence of service quality on consumer
-          satisfaction in retailing, the determinants of Fintech adoption in
-          rural India, factors determining the service quality perception in
-          India's health insurance sector, role of demographic variables on
-          consumer ethnocentrism, product judgment and willingness to buy,
-          impact of 'Start-up India' scheme on employment generation, and income
-          levels and participation of women labour force in Indian states.
+          This issue contains topics like digital lending, influence of
+          transformational leadership on employees' job performance, market
+          efficiency and volatility spillovers in the Indian commodity market,
+          influence of organisational justice on perceived organisational
+          support, influence of financial inclusion on the social status of
+          rural women, impact of the Insolvency and Bankruptcy Code, 2016,
+          digital supply chain in e-commerce, and determinants of employee
+          engagement in Indian start-ups.
         </p>
 
         <p>
-          It is over the past six months, the war has not gone well for either
-          party. The tug of war between the US-led supply of arms and
-          intelligence to Ukraine and the resourceful and resilient Russia can
-          only drag on, indefinitely. The time for resuming negotiations is now.
-          India, which has consistently held dialogue and diplomacy as the only
-          solution to take the initiative towards mediation. Prof. Shrawan Kumar
-          Singh has analysed the various aspects of conflict between Russia and
-          Ukraine.
+          In the contemporary scenario, digital lending has proved to be one of
+          the major instruments contributing to financial inclusion in India.
+          Ensuring safe, secure, reliable, accessible, affordable, and efficient
+          payment systems have been one of the important strategic objectives of
+          the RBI. Prof. Shrawan Kumar Singh writes on the framework, models,
+          growth, and regulatory mechanism of digital lending in India, and
+          cautions the readers that digital lending comes with its own set of
+          precautions.
         </p>
 
         <p>
-          Dr. Ajai Pal Sharma and Dr. Arun Yadav examine the influence of
-          service quality on customer satisfaction in the context of Indian
-          retailing, and attempt to ascertain the relationship between customer
-          satisfaction and service quality. Their study reveals that out of a
-          total of five factors, three factors of retail service quality,
-          physical appearance, personal interaction, and problem-solving
-          contribute significantly to service quality while assessing customer
-          satisfaction.
+          Nitin R. Gokarn, Dr. Praveen Kumar Mishra, and Prof. Avinash D.
+          Pathardikar examine the influence of transformational leadership on
+          job performance and the mediating role of self-efficacy. Their study
+          reveals that transformational leadership influences job performance
+          and self-efficacy positively. The study also identifies important
+          attitude-markers that may be employed as soft management techniques to
+          increase employee self-efficacy, which would improve work output for
+          the employer.
         </p>
 
         <p>
-          Dr. Smita Tripathi identifies the determinants of the adoption of
-          FinTech in selected rural areas of north India's states of
-          Uttarakhand, Uttar Pradesh, and Punjab. She also assesses the impact
-          of demographic variables on FinTech adoption. Her study has revealed
-          that the primary factors of FinTech adoption among the rural
-          population are: trust, perceived ease of use, safety, perceived risk,
-          perceived value earned, and perceived usefulness. She also finds that
-          the acceptance and experience of digital payments are influenced by
-          demographic factors, including the user's age, education, and gender.
+          Dr. Mehak Arora and Prof. Ramesh Chander examine the informational
+          efficiency of the Indian commodity market and the transmission of
+          volatility in prices of traded commodities in spot and futures
+          markets. They suggest a testament to underlying market efficiency, so
+          that futures commodity markets effectively utilise a low market price
+          acquisition platform for a seamless transmission of price sensitivity
+          to the spot markets.
         </p>
 
         <p>
-          Priyanka Kumari and Dr. Shivendra examine the factors that
-          substantially shape customers' perception of service quality in
-          India's health insurance sector. They find that six factors, namely,
-          inerrancy in receptiveness, understanding the customer, inculcating
-          certitude, infallible physical appearance, corporeal evidence, and
-          discretion towards customers, were the major factors in determining
-          the service quality perception in the health insurance.
+          Arti Negi and Dr. Reena Singh assess the influence of organisational
+          justice on perceived organisational support among the teachers of
+          higher-education institutions in the state of Uttarakhand. They find
+          that all the three dimensions of justice (distributive, procedural and
+          interactional) influence positively and significantly the perceived
+          organisational support.
         </p>
 
         <p>
-          Ankita Mandoliya and Prof. H. C. Purohit take an integrative view of
-          ethnocentrism among Indian consumers and that there is strong
-          relationship between consumer ethnocentrism, demographic antecedents,
-          consumers' product judgment, and willingness to buy. Their study
-          reveals that the Indian consumers have an ethnocentrism level below
-          average as the mean value of the majority of the items on the
-          ethnocentrism scale. This major drift of mean values towards the
-          disagreement and a minority of items getting responses above-average
-          represent a moderate to a below-moderate ethnocentric level of the
-          consumers when automobiles are taken into consideration.
+          Ritu Bharti and Prof. R. C. Dangwal assess the influence of financial
+          inclusion on the social status of rural women in Uttarakhand. They
+          used 38 scale items to draw the relevant findings using the PLS-SEM.
+          They conclude that the access, availability and the usage of financial
+          services are the significant factors influencing rural women towards
+          financial inclusion. The authors also find a significant positive
+          impact of financial inclusion on the social status of rural women in
+          Uttrakhand.
         </p>
 
         <p>
-          Resham Goyal and Prof. R. C. Dangwal assess the impact of the Central
-          Government's initiative 'Start-up India' on achieving social
-          sustainability through employment generation. They highlight the role
-          played by the Start-up India initiative in stimulating employment
-          generation across various states in India. They find a strong
-          influence of entrepreneurship on generating employment, enhancing
-          social sustainability in the country.
+          Ankita Rajput and Dr. Gautam Prasad examine the impact of the
+          Insolvency and Bankruptcy Code, 2016 (IBC) on the public-sector banks
+          in India. They find a significant impact of the enforcement of the IBC
+          on the recovery of loans, profit and non-performing assets of these
+          banks.
         </p>
 
         <p>
-          Dr. Kiran Lamba and Dr. Indra Jakhar analyse the trend of female
-          labour force participation in India. They make an interstate
-          comparison on the basis of level of income. They find that the female
-          labour force participation rate (FLFPR) has declined over the time in
-          all the states, both rural and urban areas. They argue that there is a
-          need to improve access to education and training programmes, and skill
-          development for enhancing the employability of women workers.
+          Gunjan Marwah, Dr. B. M. Gupta, and Prof. Anurag Saxena conducted a
+          bibliometric study of Digital Supply-Chain in e-Commerce, using
+          quantitative as well as qualitative indicators from the year 2000 to
+          March 2023. They find that out of the 515 global publications, China
+          and the USA turned out to be the most published countries and Hong
+          Kong Polytechnic University and Shanghai University were the Top 2
+          impactful publishing organisations during that period.
         </p>
 
         <p>
-          In the Communication section, we have two pieces. In a brief
-          communication, Prof. V.K. Shrotriya brings to light the use and abuse
-          of Power Point Presentations in the classes and conferences. The
-          author is of the view that over-dependence on PPTs is impacting the
-          teaching-learning process negatively and requires revisiting this kind
-          of pedagogy.
+          Ikshu Chauhan and Dr. Vaishali examine the key performance indicators
+          of employee engagement in Indian start-ups. They identify the key
+          performance indicators which assist the start-up business owners in
+          evaluating their employee's performance and in formulating the
+          strategies to optimise them. This might help the employers in employee
+          retention, increasing the employee satisfaction, and evaluating the
+          part played by employee engagement in start-ups.
         </p>
 
         <p>
-          Dr. Komal Nagar has provided the annotated bibliography on the works
-          of Peter F. Drucker, one of the most celebrated authors on Business
-          and Management of the 20th century. Along with the compilation of his
-          works, her communication furnishes a brief description of the life of
-          the legendary author.
+          In the communication section, we have two pieces. In a brief note,
+          Prof. V. K. Shrotriya throws a new idea and stresses the need for
+          happiness in the organisation. The author underscores the idea that if
+          the organisation's interest and focus are people-centric, it would be
+          easier to realise the dream of a happy organisation.
         </p>
 
         <p>
-          The issue also contains review of four books of current interest,
-          written by eminent experts.
+          Dr. Komal Nagar has given an annotated bibliography of the works of
+          David Ogilvy, the legendary author on advertising. A brief description
+          of the life of the author is also given.
         </p>
 
         <p>
-          I place on record my gratitude to the eminent scholars and authors for
-          their valuable contribution to the quality of our research journal,{" "}
-          <em>NICE Journal of Business</em>.
-        </p>
-
-        <p>
-          Many experts devoted their time and talent in reviewing the papers, by
-          making critical comments and suggestions for improving their quality.
-          I am deeply indebted to all of them.
+          Many experts devoted their time and talent in reviewing the
+          manuscripts, by making critical comments and suggestions for improving
+          their quality. I am deeply indebted to all of them.
         </p>
 
         <p>
@@ -150,10 +141,15 @@ function page() {
           Vice-Chancellor, of Shobhit Institute of Engineering and Technology
           (Deemed-to-be University), Meerut, for their support to this academic
           endeavour, intended to promote, preserve and disseminate business
-          research. In fact, the journal was the brainchild of Kunwar Shekhar
-          ji, when it was launched in the early 2006. Since its inception, he
-          has been taking keen interest in the growth of this research
+          research. I am proud to say that the journal was the brainchild of
+          Kunwar Shekhar ji when it was launched in 2006. Since its inception,
+          he has been taking a keen interest in the growth of this research
           publication.
+        </p>
+
+        <p>
+          Thank you for reading and your support. Any comment or feedback shall
+          be gratefully acknowledged.
         </p>
 
         <h3 className="text-right">
